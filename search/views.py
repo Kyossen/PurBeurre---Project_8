74f02eefa78380f2e2f31a2d847c513fdfef45f1
Kyossen <44588678+Kyossen@users.noreply.github.com>
2019-08-31 21:30:22 +0200

    Update
    Update

--- search/views.py
+++ search/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
@@ -8,13 +10,14 @@ from .models import *
 def index(request):
     print(request)
     context = {
+        #'connected' : user_connected
     }
     if request.method == 'POST':
         form = IngredientForm(request.POST, error_class=ParagraphErrorList)
         ingredient = form.cleaned_data['ingredient']
         result(ingredient)
         print(ingredient)
-        return render(result(ingredient), 'search/connect.html')
+        return render(request, 'search/result.html', context)
     else:
         # GET method. Create a new form to be used in the template.
         form = IngredientForm()
@@ -24,24 +27,30 @@ def index(request):
 
 def sign_up(request):
     context = {
+
     }
     if request.method == 'POST':
         form = ContactForm(request.POST, error_class=ParagraphErrorList)
         if form.is_valid():
             email = form.cleaned_data['email']
             wordpass = form.cleaned_data['wordpass']
+            wordpass_2 = form.cleaned_data['wordpass_2']
             name = form.cleaned_data['name']
             surname = form.cleaned_data['surname']
-            print(email, wordpass, name, surname)
+            print(email, wordpass, wordpass_2, name, surname)
             print('Valid')
-            new_account_db = Account(email=email,
-                                     wordpass=wordpass,
-                                     name=name,
-                                     surname=surname)
-            new_account_db.save()
-            # print(new_account_db)
-
-            print('Save in table')
+            if wordpass == wordpass_2:
+                #if cara speci :
+                user = User.objects.create_user(first_name=name, last_name=surname, username=email, password=wordpass)
+                new_account_db = Account(email=email,
+                                         wordpass=wordpass,
+                                         name=name,
+                                         surname=surname)
+                user.save()
+                new_account_db.save()
+                print('Save in table')
+            else:
+                print('Wordpass is not same')
         else:
             context['errors'] = form.errors.items()
             print('False')
@@ -53,9 +62,6 @@ def sign_up(request):
 
 
 def connect(request):
-    user_connected = request
-    print(user_connected)
-    All_accounts = Account.objects.all()
     context = {
     }
     if request.method == 'POST':
@@ -64,22 +70,15 @@ def connect(request):
             i = 0
             email = form.cleaned_data['email']
             wordpass = form.cleaned_data['wordpass']
-            user_mail = All_accounts.get(email=email)
-            user_wp = All_accounts.filter(wordpass=wordpass)
-            for find_account in All_accounts:
-                if user_mail == All_accounts[i]:
-                    try:
-                        if user_wp[i] == All_accounts[i]:
-                            print('Connected')
-                            user_connected = True
-                            stay_connect(user_connected)
+            user_connected = authenticate(username=email, password=wordpass)
+            if user_connected is not None:
+                context['email'] = email
+                print(context)
+                return render(request, 'search/dashboard.html', context)
+            else:
+                message_id_error = "Adresse email et/ou mot de passe incorrect"
+                print(message_id_error)
 
-                        else:
-                            i += 1
-
-                    except:
-                        message_id_error = "Adresse email et/ou mot de passe incorrect"
-                        print(message_id_error)
         else:
             context['errors'] = form.errors.items()
             print('False')
@@ -87,16 +86,14 @@ def connect(request):
         # GET method. Create a new form to be used in the template.
         form = ConnectForm()
     context['form'] = form
-    if user_connected is True:
-        context['connected'] = user_connected
-        return render(request, 'search/dashboard.html', context)
-    else:
-        return render(request, 'search/connect.html', context)
+    return render(request, 'search/connect.html', context)
 
 
 def dashboard(request):
-    user_connected = request
-    return render(request, 'search/dashboard.html')
+    context = {
+    }
+    print(context)
+    return render(request, 'search/dashboard.html', context)
 
 
 def favorites(request):
@@ -104,23 +101,8 @@ def favorites(request):
     return HttpResponse(template.render(request=request))
 
 
-def stay_connect(request):
+def result(request):
+    print(request)
     context = {
     }
-    user_connected = request
-    if user_connected is True:
-        context['connected'] = user_connected
-        print(context)
-    connect(user_connected)
-    print(user_connected)
-    index(user_connected)
-    dashboard(user_connected)
-    sign_up(user_connected)
-    favorites(user_connected)
-    return user_connected, context
-
-
-def result(request):
-    template = loader.get_template('search/result.html')
-    print(request)
-    return HttpResponse(template.render(request=request))
+    return render(request, 'search/result.html', context)
