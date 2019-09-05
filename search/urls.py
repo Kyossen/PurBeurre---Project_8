667ba8a6ae2d9836e1dde9eb6eac9ac712a5e928
Kyossen <44588678+Kyossen@users.noreply.github.com>
2019-09-05 22:11:17 +0200

    Update
    Update

--- search/urls.py
+++ search/urls.py
@@ -4,8 +4,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.connect, name="connect"),
-    url(r'^$', views.dashboard, name="dashboard"),
-    url(r'^search/templates/', views.favorites, name="favorites"),
-    url(r'^search/templates/', views.sign_up, name="sign_up"),
-    url(r'^search/templates/', views.result, name="result"),
+    url(r'^templates/sign_up.html', views.sign_up, name="sign_up"),
+    url(r'^templates/dashboard.html', views.dashboard, name="dashboard"),
+    url(r'^templates/favorites.html', views.favorites, name="favorites"),
+    url(r'^templates/result.html', views.result, name="result"),
+    url(r'^templates/disconnect.html', views.disconnect, name="disconnect"),
 ]
+
