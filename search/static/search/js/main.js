d25eee007bdb6a57f0119d814cb56cf6f91e83aa
Kyossen <44588678+Kyossen@users.noreply.github.com>
2019-09-11 16:21:33 +0200

    Update
    Update

--- search/static/search/js/main.js
+++ search/static/search/js/main.js
@@ -1,6 +1,10 @@
-$(document).ready(function(){
-    $('#account').on("click", function(page){
-        console.log("True")
-        location.href="sign_up.html";
-     });
-});
+function connect() {
+/* Disable the button to ensure that the query is not repeated in a loop */
+   $("#button_connect").attr("disabled", true);
+   $('#loading').append('<p> Connexion en cours... </p>');
+   $('#spinner').toggle();
+    if (document.getElementById("loading")) {
+            setTimeout(connect, 4000);
+            $("#button_connect").attr("disabled", false);
+          }
+}
