7d2ddc23b3a0b638698b3264cf5537e8036edca2
Kyossen <44588678+Kyossen@users.noreply.github.com>
2019-09-17 18:25:00 +0200

    Update
    Update

--- search/static/search/js/main.js
+++ search/static/search/js/main.js
@@ -1,10 +1,43 @@
+$( document ).ready(function() {
+        console.log( "document loaded" );
+        $( "#target" ).click(function() {
+            console.log( "Handler for .click() called." );
+            /* Le bouton du haut -> id : find_top
+            Celui du millieux -> id : find_mid*/
+        });
+});
+
 function connect() {
-/* Disable the button to ensure that the query is not repeated in a loop */
-   $("#button_connect").attr("disabled", true);
-   $('#loading').append('<p> Connexion en cours... </p>');
-   $('#spinner').toggle();
-    if (document.getElementById("loading")) {
-            setTimeout(connect, 4000);
-            $("#button_connect").attr("disabled", false);
-          }
+    // $("#button_connect").attr("disabled", true);
+    $('#spinner').show( 600 );
+    $('#spinner').hide(10250)
+    // setTimeout(button_activate, 10000);
 }
+
+function button_activate() {
+    $("#button_connect").attr("disabled", false);
+}
+/*
+    if (document.getElementById('id_email')) {
+        $(id_email).change(function () {
+             var username = $('#id_email').val();
+             $(id_wordpass).change(function () {
+                var password = $(id_wordpass).val();
+
+                $.ajax({
+                    url: 'ajax/validate_connect/',
+                    data: {
+                    'username': username,
+                    'password': password
+                    },
+                    dataType: 'json',
+                    success: function (data) {
+                    if (data.is_taken) {
+                    console.log(data)
+
+                        }
+                    }
+      });
+    });
+    });
+*/
