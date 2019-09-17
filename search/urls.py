e71b23764930a74230b5f66435ca456b0279a1d9
Kyossen <44588678+Kyossen@users.noreply.github.com>
2019-09-17 18:26:07 +0200

    Update
    Update

--- search/urls.py
+++ search/urls.py
@@ -3,11 +3,14 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.connect, name="connect"),
-    url(r'^templates/sign_up.html', views.sign_up, name="sign_up"),
-    url(r'^templates/dashboard.html', views.dashboard, name="dashboard"),
-    url(r'^templates/favorites.html', views.favorites, name="favorites"),
-    url(r'^templates/result.html', views.result, name="result"),
-    url(r'^templates/disconnect.html', views.disconnect, name="disconnect"),
+    url(r'^connect.html', views.connect, name="connect"),
+    # url(r'^ajax/validate_connect/$', views.validate_connect, name='validate_connect'),
+    url(r'^sign_up.html', views.sign_up, name="sign_up"),
+    url(r'^dashboard.html', views.dashboard, name="dashboard"),
+    url(r'^favorites.html', views.favorites, name="favorites"),
+    url(r'^result.html', views.result, name="result"),
+    url(r'^disconnect.html', views.disconnect, name="disconnect"),
+    url(r'^description.html', views.description, name="description"),
 ]
+
 
