f17692f5f12ff02f8b63e22769be25e48182b186
Kyossen <44588678+Kyossen@users.noreply.github.com>
2019-09-26 18:07:36 +0200

    Update
    Update

--- search/static/search/js/main.js
+++ search/static/search/js/main.js
@@ -1,3 +1,37 @@
+/* It's a small function for to spawn print of the spinner */
+
 function connect() {
     $('#spinner').show( 600 );
 }
+
+
+/* 
+The function below record a click of the user
+She records the click that selects a non-registered favorite food and will indicate it to the system to create the record
+The advantage of going through this function is to stay on the page and to be able to record other foods simultaneously thanks to Ajax 
+*/
+
+$( document ).ready(function() {
+    $(check_saveFood).click(function() {
+        var product = $( this ).val();
+        console.log(product)
+        console.log('True');
+
+          $.ajax({
+            url: '/search/favorites.html',
+            data: {
+              'product': product
+            },
+            dataType: 'json',
+            success: function (data) {
+              if (data.error_food) {
+                $('#error_result').append('<p>' + data.error_food + '</p>');
+              }
+              if (data.success_save) {
+                $('#error_result').append('<p>' + data.success_save + '</p>');
+              }
+            }
+          });
+      })
+});
+
