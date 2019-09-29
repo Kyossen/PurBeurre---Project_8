dd5090cf683ffcc880bcbc51162fb8871c3e1b6c
Kyossen <44588678+Kyossen@users.noreply.github.com>
2019-09-29 15:29:45 +0200

    Update
    Update

--- search/models.py
+++ search/models.py
@@ -1,8 +1,23 @@
+"""
+This import is obligatory for the good of the system
+"""
+
 from django.db import models
 from django.contrib.auth.models import User
 
+"""
+This below, the all models of the platform
+"""
 
-# Create your models here.
+
+"""
+The first model is the model Account
+She create a foreign key on the User table
+This key is used to add objects (such as a phone number)
+    to the user in the User table.
+"""
+
+
 class Account(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE,
                              related_name="User",
@@ -21,6 +36,16 @@ class Account(models.Model):
         managed = True
         db_table = "Account"
         ordering = ['id']
+
+
+"""
+The second model is the model Substitution
+It also create a foreign key on the User table
+This key is used to add a substitution
+This substitution is used to find which user has registered a product
+Thus, a user can register a product in this table
+    to display it in these favorite products
+"""
 
 
 class Substitution(models.Model):
