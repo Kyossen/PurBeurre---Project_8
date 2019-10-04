ddb7ba3b7e676ef877afbb6b97a6a26ac8cf5ce9
Kyossen <44588678+Kyossen@users.noreply.github.com>
2019-10-04 23:06:23 +0200

    Update
    Updae

--- search/static/search/js/main.js
+++ search/static/search/js/main.js
@@ -1,41 +1,43 @@
 /* It's a small function for to spawn print of the spinner */
 function connect() {
-    $('#spinner').show( 600 );
+    $('#spinner').show(600);
 }
 
 
 /* These code line use js for change the input on page top */
-document.getElementById('id_food'); {
-    $('#id_food').toggleClass("color_find_base")
+document.getElementById('id_food');
+{
+    $('#id_food').toggleClass("color_find_base");
 }
 
 
-/*
-The function below record a click of the user
-She records the click that selects a non-registered favorite food and will indicate it to the system to create the record
-The advantage of going through this function is to stay on the page and to be able to record other foods simultaneously thanks to Ajax
-*/
-$( document ).ready(function() {
-    $(check_saveFood).click(function() {
-        var product = $( this ).val();
-        console.log(product)
-        console.log('True');
+/* The function below record a click of the user
+She records the click that selects a non-registered
+    favorite food and will indicate it to the system to create the record */
+$(document).on("click", "#check_saveFood", function () {
+    var product = $(this).val();
+    $(this).toggleClass('orange_back');
+    console.log(product);
+    console.log('True');
 
-          $.ajax({
-            url: '/search/favorites.html',
-            data: {
-              'product': product
-            },
-            dataType: 'json',
-            success: function (data) {
-              if (data.error_food) {
-                $('#error_result').append('<p>' + data.error_food + '</p>');
-              }
-              if (data.success_save) {
-                $('#error_result').append('<p>' + data.success_save + '</p>');
-              }
+    $.ajax({
+        url: '/search/favorites.html',
+        data: {
+            'product': product
+        },
+        dataType: 'json',
+        success: function (data) {
+            if (data['error_food']) {
+                if (!document.getElementById("result_error")) {
+                    $('#error_result').append('<p id="result_error" class="result_error">' + data['error_food'] + '</p>');
+                }
             }
-          });
-      })
-});
+            if (data['success_save']) {
+                if (!document.getElementById("result_success")) {
+                    $('#error_result').append('<p id="result_success" class="result_success">' + data['success_save'] + '</p>');
 
+                }
+            }
+        }
+    });
+});
