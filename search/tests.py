6fad0ede2d4b8a52aebf2e0add110c0924a5ba36
Kyossen <44588678+Kyossen@users.noreply.github.com>
2019-11-29 12:56:37 +0100

    Update
    Update

--- search/tests.py
+++ search/tests.py
@@ -15,7 +15,10 @@ from .models import Product, Categories
 class IndexPageTestCase(TestCase):
     """This class tests whether the index
     page returns a 200 status code"""
+
     def test_index_page(self):
+        """Test the home page (index)"""
+        print("Test the home page (index)")
         response = self.client.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
 
@@ -23,7 +26,10 @@ class IndexPageTestCase(TestCase):
 class CopyrightPageTestCase(TestCase):
     """This class tests whether the copyright
     page returns a 200 status code"""
+
     def test_copyright_page(self):
+        """Test the copyright page"""
+        print("Test the copyright page")
         response = self.client.get(reverse('copyright'))
         self.assertEqual(response.status_code, 200)
 
@@ -33,7 +39,9 @@ class ResultPageTestCase(TestCase):
     returns a 200 status code if a food is found or not"""
 
     def test_result_find_page_returns_200(self):
-        """Test if food is find"""
+        """This method create a food in the database
+        and test if user can the find this food"""
+        print("Test for find a food. Food is found.")
         categories = Categories.objects.create(name='TestCategories',
                                                url='test')
         categories_id = Categories.objects.get(pk=categories.pk)
@@ -48,6 +56,7 @@ class ResultPageTestCase(TestCase):
 
     def test_result_page_returns_notExist(self):
         """Test if food not is find or not exist"""
+        print("Test for find a food. Food is not found.")
         response = self.client.post(reverse('result'), {'food': 'NotExist'})
         self.assertEqual(response.status_code, 401)
 
@@ -59,6 +68,7 @@ class DescriptionPageTestCase(TestCase):
 
     def test_description_find_page_returns_200(self):
         """Test with a good code"""
+        print("Test for description page with a good code")
         categories = Categories.objects.create(name='TestCategories',
                                                url='test')
         categories_id = Categories.objects.get(pk=categories.pk)
@@ -74,6 +84,7 @@ class DescriptionPageTestCase(TestCase):
 
     def test_description_page_returns_badCode(self):
         """Test with a bad code"""
+        print("Test for description page with a bad code")
         response = self.client.get(reverse('description'),
                                    {'product': 'BadCode'})
         self.assertEqual(response.status_code, 200)
@@ -86,6 +97,7 @@ class FavoritesPageTestCase(TestCase):
 
     def test_Favorites_find_page_returns_200(self):
         """Test with a good code"""
+        print("Test for favorites page with a good code")
         categories = Categories.objects.create(name='TestCategories',
                                                url='test')
         categories_id = Categories.objects.get(pk=categories.pk)
@@ -101,6 +113,7 @@ class FavoritesPageTestCase(TestCase):
 
     def test_favorites_page_returns_badCode(self):
         """Test with a bad code"""
+        print("Test for favorites page with a bad code")
         response = self.client.get(reverse('favorites'),
                                    {'product': 'BadCode'})
         self.assertEqual(response.status_code, 200)
